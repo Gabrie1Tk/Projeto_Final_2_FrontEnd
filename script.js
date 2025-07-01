5bf206d15fa66ced557637dc84ce11d666afccc4
Gabriel Takao <83586924+Gabrie1Tk@users.noreply.github.com>
2025-07-01 08:42:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirExemplo1(){
+    window.location.href='Exemplo_1/index.html';
+}
+
+function abrirExemplo2(){
+    window.location.href='Exemplo_2/index.html';
+}
+
+function abrirExemplo3(){
+    window.location.href='Exemplo_3/index.html';
+}
